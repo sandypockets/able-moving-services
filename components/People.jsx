133e5ba06ffef45d603e5ff61eae4386a9100f34
sandypockets <zac@znacreative.com>
2021-05-30 01:27:58 -0400

    Create initial components

---
+++ components/People.jsx
@@ -0,0 +1,59 @@
+/* This example requires Tailwind CSS v2.0+ */
+const people = [
+  {
+    name: 'Bob Bobson',
+    role: 'Owner',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Ty Tyson',
+    role: 'Crew Supervisor',
+    imageUrl:
+      'https://images.unsplash.com/photo-1528763380143-65b3ac89a3ff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Matt Mattson',
+    role: 'Mover',
+    imageUrl:
+      'https://images.unsplash.com/photo-1601455763557-db1bea8a9a5a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Sam Samson',
+    role: 'Mover',
+    imageUrl:
+      'https://images.unsplash.com/photo-1546820389-44d77e1f3b31?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+]
+
+export default function People() {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+        <div className="grid grid-cols-1 gap-12 lg:grid-cols-3 lg:gap-8">
+          <div className="space-y-5 sm:space-y-4">
+            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">Meet your support team</h2>
+            <p className="text-xl text-gray-500">
+              No matter the move, we'll get your stuff there safe and sound.
+            </p>
+          </div>
+          <div className="lg:col-span-2">
+            <ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-12 sm:space-y-0 lg:gap-x-8">
+              {people.map((person) => (
+                <li key={person.name}>
+                  <div className="flex items-center space-x-4 lg:space-x-6">
+                    <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src={person.imageUrl} alt="" />
+                    <div className="font-medium text-lg leading-6 space-y-1">
+                      <h3>{person.name}</h3>
+                      <p className="text-indigo-600">{person.role}</p>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
