e4bb08789a826befdaa9a88c14bcd543b85035c1
sandypockets <zac@znacreative.com>
2021-05-30 15:20:39 -0400

    Create features list component outline

---
+++ components/FeatureList.jsx
@@ -0,0 +1,63 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { CheckIcon } from '@heroicons/react/outline'
+
+const features = [
+  {
+    name: 'Full homes',
+    description: 'No matter how many bedrooms, we can get your stuff there. Safe and sound.',
+  },
+  {
+    name: 'Offices',
+    description: 'Get your equipment to where it needs to be.',
+  },
+  {
+    name: 'City to City',
+    description: 'Across town or across the province, we\'ve got you covered.',
+  },
+  {
+    name: 'Pack-safe',
+    description: 'We\'ll get your things from A to B in one piece.',
+  },
+  {
+    name: 'Neighbourly',
+    description: 'Magna a vel sagittis aliquam eu amet. Et lorem auctor quam nunc odio. Sed bibendum.',
+  },
+  {
+    name: 'Locally Owned',
+    description: 'Magna a vel sagittis aliquam eu amet. Et lorem auctor quam nunc odio. Sed bibendum.',
+  },
+  {
+    name: 'Instruments',
+    description: 'Need to move a piano? We can get it there in tune.',
+  },
+  {
+    name: 'Reasonably Priced',
+    description: 'A safe move, that won\'t break the bank.',
+  },
+]
+
+export default function Example() {
+  return (
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
+        <div className="max-w-3xl mx-auto text-center">
+          <h2 className="text-3xl font-extrabold text-gray-900">We'll get you there.</h2>
+          <p className="mt-4 text-lg text-gray-500">
+            Whether you're moving your family to a new home, relocating offices, or need to get a priceless piano out of your basement, we're here.
+          </p>
+        </div>
+        <dl className="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-4 lg:gap-x-8">
+          {features.map((feature) => (
+            <div key={feature.name} className="relative">
+              <dt>
+                <CheckIcon className="absolute h-6 w-6 text-green-500" aria-hidden="true" />
+                <p className="ml-9 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
+              </dt>
+              <dd className="mt-2 ml-9 text-base text-gray-500">{feature.description}</dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </div>
+  )
+}
