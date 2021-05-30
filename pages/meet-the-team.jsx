197c7e0bdf52ecea389d0e23dd8ef20ab9b440de
sandypockets <zac@znacreative.com>
2021-05-30 01:28:11 -0400

    Create meet the team page

---
+++ pages/meet-the-team.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero";
+import Footer from "../components/Footer";
+import People from "../components/People";
+
+export default function MeetTheTeam() {
+  return (
+    <div>
+      <People />
+    </div>
+  )
+}
