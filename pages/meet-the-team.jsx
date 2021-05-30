5a44cef10651997761a67c9db51a159c9ed87a42
sandypockets <zac@znacreative.com>
2021-05-30 01:35:29 -0400

    Add nav, people content

--- pages/meet-the-team.jsx
+++ pages/meet-the-team.jsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import People from "../components/People";
+import SecondaryNav from '../components/SecondaryNav'
 
 export default function MeetTheTeam() {
   return (
     <div>
+      <SecondaryNav />
       <People />
     </div>
   )
