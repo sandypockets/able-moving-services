c384dc674d116ec71fe884d4591c0c02f1026d62
sandypockets <zac@znacreative.com>
2021-05-31 01:09:55 -0400

    Swap contact page component

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,12 +1,14 @@
 import ContactForm from '../components/ContactForm'
 import SecondaryNav from '../components/SecondaryNav'
 import Footer from "../components/Footer";
+import ContactDetails from "../components/ContactDetails";
 
 export default function Contact() {
   return (
     <div>
       <SecondaryNav />
-      <ContactForm />
+      {/*<ContactForm />*/}
+      <ContactDetails />
       <Footer />
     </div>
   )
