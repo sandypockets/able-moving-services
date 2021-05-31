1f7ca9e1535b7ccf4657b46a898266a7c400c105
sandypockets <zac@znacreative.com>
2021-05-31 00:28:54 -0400

    Create starter reviews page

---
+++ pages/reviews.jsx
@@ -0,0 +1,13 @@
+import SecondaryNav from '../components/SecondaryNav'
+import Testimonial from '../components/Testimonial'
+import Footer from '../components/Footer'
+
+export default function Reviews() {
+  return (
+    <div>
+      <SecondaryNav />
+      <Testimonial />
+      <Footer />
+    </div>
+  )
+}
