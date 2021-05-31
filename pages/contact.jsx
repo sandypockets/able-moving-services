947e085d75c622448f8045209f3ec2754556c43a
sandypockets <zac@znacreative.com>
2021-05-31 00:28:37 -0400

    Create contact page with form

---
+++ pages/contact.jsx
@@ -0,0 +1,13 @@
+import ContactForm from '../components/ContactForm'
+import SecondaryNav from '../components/SecondaryNav'
+import Footer from "../components/Footer";
+
+export default function Contact() {
+  return (
+    <div>
+      <SecondaryNav />
+      <ContactForm />
+      <Footer />
+    </div>
+  )
+}
