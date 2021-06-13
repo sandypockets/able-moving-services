a922a6dd5eae3be349851671848afa03b371fd2c
sandypockets <zac@znacreative.com>
2021-06-13 14:51:41 -0400

    Update name

--- components/People.jsx
+++ components/People.jsx
@@ -1,6 +1,6 @@
 const people = [
   {
-    name: 'Bob Bobson',
+    name: 'Lyn Lynson',
     role: 'Owner',
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
