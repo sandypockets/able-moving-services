f041fa9d6127241d0f7b8aa86f2623f9fa4a0c44
sandypockets <zac@znacreative.com>
2021-06-13 14:41:39 -0400

    Tidy comments

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,22 +1,4 @@
-/*
-  This example requires Tailwind CSS v2.0+
-
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ]
-  }
-  ```
-*/
-
 import form from '@tailwindcss/forms'
-
 
 export default function ContactForm() {
   return (
