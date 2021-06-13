6a587a4e1e39d4ff459fab5e39c3c2dacfc3113f
sandypockets <zac@znacreative.com>
2021-06-13 14:43:13 -0400

    Tidy comments

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,4 +1,3 @@
-/* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
