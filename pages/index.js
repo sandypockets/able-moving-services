652fc7871a5ed3b9133ed091f186fa6743a9f145
sandypockets <zac@znacreative.com>
2021-06-13 14:48:30 -0400

    Remove unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Nav from '../components/Nav'
 import Hero from '../components/Hero'
 import Footer from '../components/Footer'
 import FeatureList from '../components/FeatureList'
